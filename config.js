a5cc8b4d8c2b73cad304324800ced1c933477a5e
AiSED Conference <info@aisedconference.org>
2026-06-25 15:42:56 +0800

    Link registration form to new master sheet

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ window.AISED_FORMS = {
 };
 
 window.AISED_CONFIG = {
-  registrationEndpoint: "https://script.google.com/macros/s/AKfycbwjGauDQniCe6dn5JM-rsuiKEpeAaEKRzqBpvXbr6uBK6qZ6aNgXfNwRkUaQEkubzd4/exec"
+  registrationEndpoint: "https://script.google.com/macros/s/AKfycbyaKXC4mCnHH_y4IO23-sZzAV2K1RIgyqzgZoWcC73N_x93rDec9rXuyLs_neY80eEISA/exec"
 };
